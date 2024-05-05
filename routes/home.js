43ee63c2ff0f98fc2eff456607e02ba07fa29760
alonemo <kamishnikov_da@niuitmo.ru>
2024-05-05 15:04:05 +0300

    micro fix

--- routes/home.js
+++ routes/home.js
@@ -4,50 +4,62 @@ const servos = [
   {
     name: 'Servo-1',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-2',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-3',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-4',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-5',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-6',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-7',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-8',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-9',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-10',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-11',
     position: 0,
+    speed: 0,
   },
   {
     name: 'Servo-12',
     position: 0,
+    speed: 0,
   },
 ];
 
@@ -62,6 +74,7 @@ router.get("/", async (req, res, next) => {
 router.post("/moveServo", async (req,res) => {
   for(let i = 0; i < servos.length; i++){
     servos[i].position = req.body.servos[i].position;
+    servos[i].speed = req.body.servos[i].speed;
   }
   return res.status(200).json({message: "OK", servos: servos});
 });
